191a5471598756c86cf211a59c20668417fd16d6
zubeyirctr <zubeyircatar.19@gmail.com>
2026-05-18 22:14:09 +0300

    uygulama yapılandırması güncellendi, auth ve CORS entegrasyonu eklendi

--- src/app.js
+++ src/app.js
@@ -1,29 +1,31 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 
 const swaggerDocument = YAML.load(path.join(__dirname, "swagger.yaml"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
 app.use(express.static(path.join(__dirname, "../public")));
 
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
 const propertyRoutes = require("./routes/propertyRoutes");
 app.use("/api/properties", propertyRoutes);
-
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor...`);
   console.log(`Dokümantasyon: http://localhost:${PORT}/api-docs`);
